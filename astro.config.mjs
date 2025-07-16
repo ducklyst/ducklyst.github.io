9fa7421dbfd2b5e60d396397ba68dab88d628979
Kaushik <219108718+ducklyst@users.noreply.github.com>
2025-07-17 01:11:31 +0530

    add github page url in astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ import mdx from '@astrojs/mdx';
 // https://astro.build/config
 export default defineConfig({
   integrations: [svelte(), mdx()],
-
+  site: 'https://ducklyst.github.io',
   vite: {
     plugins: [tailwindcss()]
   }
