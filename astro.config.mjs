a433802cf86c66c14d894153185995464cf4600a
Kaushik <219108718+ducklyst@users.noreply.github.com>
2025-07-17 01:08:21 +0530

    init, setup astro with tailwind and fontsource and setup startpage

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,14 @@ import { defineConfig } from 'astro/config';
 
 import svelte from '@astrojs/svelte';
 
+import tailwindcss from '@tailwindcss/vite';
+import mdx from '@astrojs/mdx';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte()]
+  integrations: [svelte(), mdx()],
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
